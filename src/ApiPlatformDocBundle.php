73e1275c1fc4927442e37091433455ee7eef27b8
Gianni GIUDICE <gg@vitalytech.com>
2023-08-07 16:16:03 +0200

    :bug:

--- src/ApiPlatformDocBundle.php
+++ src/ApiPlatformDocBundle.php
@@ -4,8 +4,10 @@
 namespace VTGianni\ApiPlatformDocBundle;
 
 
+use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use VTGianni\ApiPlatformDocBundle\DependencyInjection\ApiPlatformDocExtension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 
 class ApiPlatformDocBundle extends Bundle
@@ -17,5 +19,11 @@ class ApiPlatformDocBundle extends Bundle
         }
 
         return $this->extension;
+    }
+
+    public function build(ContainerBuilder $builder)
+    {
+        parent::build($builder);
+        $builder->addResource(new FileResource($this->getPath().'/Resources/config/routing.yaml'));
     }
 }
